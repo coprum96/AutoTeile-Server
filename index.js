0ea84c79f890486a75cb49b34d19899c6051356f
coprum96 <medn@list.ru>
2023-03-27 12:05:49 +0300

    dotenv, server start

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express()
+const cors = require('cors')
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv')
+dotenv.config();
 const port = process.env.PORT || 5000;
 
 app.get('/', async (req, res) => {
-    res.send('hello world');
+    res.send('Fuck you');
  });
 
 app.listen(port, async (req, res) => {
