17b0e8a9ad1e9e421b97a76e97adc418e90c9216
coprum96 <medn@list.ru>
2023-04-02 15:08:17 +0300

    port 3000

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
