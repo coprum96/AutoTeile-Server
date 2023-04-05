f96e2703efbc67e28b739f48017b54ff5d0219d9
coprum96 <medn@list.ru>
2023-04-05 18:09:25 +0300

    orders, admin, products

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -30,6 +30,7 @@ function verifyJWT(req, res, next) {
    });
 }
 
+//DATABASE
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, {
    useNewUrlParser: true,
@@ -40,17 +41,182 @@ const client = new MongoClient(uri, {
 const run = async () => {
    try {
       await client.connect();
-      console.log('MongoDB connected');
-
+      console.log('MongoDB is connected');
+      const PartsCollection = client.db('AutoTeile').collection('parts');
       const UsersCollection = client.db('AutoTeile').collection('users');
+      const OrdersCollection = client.db('AutoTeile').collection('orders');
+      const PaymentCollection = client.db('AutoTeile').collection('payment');
+      const reviewsCollection = client.db('AutoTeile').collection('reviews');
 
-      // GET ALL USERS
+      const verifyAdmin = async (req, res, next) => {
+         const requester = req.decoded.email;
+         const account = await UsersCollection.findOne({
+            email: requester,
+         });
+         if (account.role === 'admin') {
+            next();
+         } else {
+            res.status(403).send({ message: 'forbidden' });
+         }
+      };
+
+      // //PAYMENT
+      // app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+      //    const { total } = req.body;
+      //    const amount = total * 100;
+
+      //    const paymentIntent = await stripe.paymentIntents.create({
+      //       amount: amount,
+      //       currency: 'usd',
+      //       payment_method_types: ['card'],
+      //    });
+      //    res.send({ clientSecret: paymentIntent.client_secret });
+      // });
+
+      // ROUTES
+
+      // GET ALL PARTS
+      app.get('/parts', async (req, res) => {
+         const parts = await PartsCollection.find({}).toArray();
+         res.send(parts);
+      });
+
+      //ADD NEW PARTS
+      app.post('/parts', verifyJWT, verifyAdmin, async (req, res) => {
+         const part = req.body;
+         const result = await PartsCollection.insertOne(part);
+         res.send(result);
+      });
+      //DELETE PARTS
+      app.delete('/parts/:id', verifyJWT, verifyAdmin, async (req, res) => {
+         const id = req.params.id;
+         const deleted = await PartsCollection.deleteOne({
+            _id: ObjectId(id),
+         });
+         res.send(deleted);
+         console.log(deleted);
+      });
+
+      //GET PART BY ID
+      app.get('/parts/:id', verifyJWT, async (req, res) => {
+         const part = await PartsCollection.findOne({
+            _id: ObjectId(req.params.id),
+         });
+         res.send(part);
+      });
+
+      //REVIEW
+
+      //POST REVIEW
+      app.post('/reviews', verifyJWT, async (req, res) => {
+         const review = req.body;
+         const result = await reviewsCollection.insertOne(review);
+         res.send(result);
+      });
+      //GET ALL REVIEWS
+      app.get('/reviews', async (req, res) => {
+         const reviews = await reviewsCollection.find({}).toArray();
+         res.send(reviews);
+      });
+
+      // ORDERS
+
+      //GET ALL ORDERS
+      app.get('/orders', verifyJWT, async (req, res) => {
+         const orders = await OrdersCollection.find({}).toArray();
+         res.send(orders);
+      });
+
+      //GET  ORDER BY USER EMAIL
+      app.get('/orders/:email', verifyJWT, async (req, res) => {
+         const orders = await OrdersCollection.find({
+            email: req.params.email,
+         }).toArray();
+
+         res.send(orders);
+      });
+
+      //UPDATE ORDER STATUS
+
+      app.patch('/orders/:id', verifyJWT, async (req, res) => {
+         const id = req.params.id;
+         const payment = req.body;
+         const filter = { _id: ObjectId(id) };
+         const updatedDoc = {
+            $set: {
+               paid: true,
+               transactionId: payment.transactionId,
+            },
+         };
+
+         const result = await PaymentCollection.insertOne(payment);
+         const updatedBooking = await OrdersCollection.updateOne(filter, updatedDoc);
+         res.send(updatedBooking);
+      });
+
+      //DELETE ORDER
+      app.delete('/orders/:id', verifyJWT, async (req, res) => {
+         const id = req.params.id;
+         const deleted = await OrdersCollection.deleteOne({
+            _id: ObjectId(id),
+         });
+         res.send(deleted);
+      });
+
+      //GET ORDER BY ID
+      app.get('/order/:id', verifyJWT, async (req, res) => {
+         const id = req.params.id;
+
+         const product = await OrdersCollection.findOne({
+            _id: ObjectId(id),
+         });
+
+         res.send(product);
+      });
+
+      //UPDATE ORDER SHIPPING STATUS
+      app.put('/orders/:id', verifyJWT, async (req, res) => {
+         const status = req.body;
+         const id = req.params.id;
+         const filter = { _id: ObjectId(id) };
+
+         const updateDoc = {
+            $set: status,
+         };
+         const result = await OrdersCollection.updateOne(filter, updateDoc);
+         res.send(result);
+      });
+
+      //POST ORDER BY EMAIL
+      //?USER CAN NOT ORDER SAME PRODUCT TWICE
+      app.post('/orders', verifyJWT, async (req, res) => {
+         const order = req.body;
+         const query = {
+            productId: order.productId,
+            email: order.email,
+         };
+         const exist = await OrdersCollection.findOne(query);
+
+         if (exist?.productId === order?.productId && exist?.email === order?.email) {
+            return res.send({
+               success: false,
+               message: 'Order already exist',
+            });
+         }
+         const result = await OrdersCollection.insertOne(order);
+
+         return res.send({ success: true, result });
+      });
+
+      //USERS
+
+      //GET ALL USERS
       app.get('/users', verifyJWT, async (req, res) => {
          const users = await UsersCollection.find({}).toArray();
          res.send(users);
       });
 
-      // UPDATE OR CREATE USER
+      //UPDATE OR CREATE USER
       app.put('/users/:email', async (req, res) => {
          const user = req.body;
          const email = req.params.email;
@@ -60,13 +226,13 @@ const run = async () => {
             $set: user,
          };
          const result = await UsersCollection.updateOne(filter, updateDoc, options);
-         const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+         const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
             expiresIn: '30d',
          });
          res.send({ result, accessToken });
       });
 
-      // GET USER BY EMAIL
+      //GET USER BY EMAIL
       app.get('/users/:email', verifyJWT, async (req, res) => {
          const user = await UsersCollection.findOne({
             email: req.params.email,
@@ -74,8 +240,36 @@ const run = async () => {
          res.send(user);
       });
 
+      //ADMIN
+
+      // Login endpoint
+      app.post('/login', async (req, res) => {
+      // Authenticate user here and generate JWT token
+      const token = jwt.sign({ email: 'medn@list.ru' }, 'your-secret-key');
+      res.json({ token });
+      });
+      
+      //MAKE ADMIN
+      app.put('/users/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+         const email = req.params.email;
+         const filter = { email: email };
+         const updateDoc = {
+            $set: { role: 'admin' },
+         };
+         const result = await UsersCollection.updateOne(filter, updateDoc);
+         res.send(result);
+      });
+
+      //GET ADMIN
+      app.get('/admin/:email', async (req, res) => {
+         const email = req.params.email;
+         const user = await UsersCollection.findOne({ email: email });
+         const isAdmin = user.role === 'admin';
+         res.send({ admin: isAdmin });
+      });
    } catch (error) {
-      console.error(error);
+      console.log(error);
+   } finally {
    }
 };
 
