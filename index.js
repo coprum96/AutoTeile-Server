78e916df47d344fe9511485ea6be84f356768a51
coprum96 <medn@list.ru>
2023-04-06 18:02:02 +0300

    payment, stripe, admin

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-dotenv.config();
-
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+//PORT
 const port = process.env.PORT || 5000;
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-// JWT verification middleware
+//  jwt verification
 function verifyJWT(req, res, next) {
    const authHeader = req.headers.authorization;
    if (!authHeader) {
-      return res.status(401).send({ message: 'Unauthorized access' });
+      return res.status(401).send({ message: 'unauthorized access' });
    }
    const token = authHeader.split(' ')[1];
-   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
       if (err) {
-         return res.status(403).send({ message: 'Forbidden access' });
+         return res.status(403).send({ message: 'can not go further!!😕 forbidden access' });
       }
 
       req.decoded = decoded;
@@ -48,37 +46,34 @@ const run = async () => {
       const PaymentCollection = client.db('AutoTeile').collection('payment');
       const reviewsCollection = client.db('AutoTeile').collection('reviews');
 
-      async function verifyAdmin(req, res, next) {
+      const verifyAdmin = async (req, res, next) => {
          const requester = req.decoded.email;
-         const account = await UsersCollection.findOne({ email: requester });
-         if (account.email === "medn@list.ru") {
-           const filter = { email: "medn@list.ru" };
-           const updateDoc = { $set: { role: 'admin' } };
-           await UsersCollection.updateOne(filter, updateDoc);
-           next();
-         } else if (account.role === "admin") {
-           next();
+         const account = await UsersCollection.findOne({
+            email: requester,
+         });
+         if (account.role === 'admin') {
+            next();
          } else {
-           res.status(403).send({ message: "forbidden" });
+            res.status(403).send({ message: 'forbidden' });
          }
-       }
+      };
 
-      // //PAYMENT
-      // app.post('/create-payment-intent', verifyJWT, async (req, res) => {
-      //    const { total } = req.body;
-      //    const amount = total * 100;
+      //PAYMENT
+      app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+         const { total } = req.body;
+         const amount = total * 100;
 
-      //    const paymentIntent = await stripe.paymentIntents.create({
-      //       amount: amount,
-      //       currency: 'usd',
-      //       payment_method_types: ['card'],
-      //    });
-      //    res.send({ clientSecret: paymentIntent.client_secret });
-      // });
+         const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: 'usd',
+            payment_method_types: ['card'],
+         });
+         res.send({ clientSecret: paymentIntent.client_secret });
+      });
 
-      // ROUTES
+      //ROUTES
 
-      // GET ALL PARTS
+      //GET ALL PARTS
       app.get('/parts', async (req, res) => {
          const parts = await PartsCollection.find({}).toArray();
          res.send(parts);
@@ -244,7 +239,7 @@ const run = async () => {
       });
 
       //ADMIN
-      
+
       //MAKE ADMIN
       app.put('/users/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
          const email = req.params.email;
@@ -272,9 +267,9 @@ const run = async () => {
 run().catch(console.dir);
 
 app.get('/', async (req, res) => {
-    res.send('Hello World');
- });
+   res.send('hello world');
+});
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
- });
+app.listen(port, async (req, res) => {
+   console.log(`Server is running in ${port}`);
+});
